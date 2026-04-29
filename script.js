3fab280e449e6cddbe6705359eae5453470d27d5
cetijosemarques2-star <cetijosemarques2@gmail.com>
2026-04-29 10:42:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-function abrir(secao) {
-  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
-  document.getElementById(secao).classList.add('active');
-}
-
-function enviar() {
-  alert("Resposta enviada!");
-}
-
 function enviarMsg() {
   let msg = document.getElementById("msg").value;
   let chat = document.getElementById("chatBox");
 
+  if (msg.trim() === "") return;
+
+  // Nome
+  let nome = document.createElement("div");
+  nome.classList.add("nome");
+  nome.textContent = "Anônimo";
+
+  // Mensagem
   let div = document.createElement("div");
   div.classList.add("msg", "aluno");
   div.textContent = msg;
 
+  chat.appendChild(nome);
   chat.appendChild(div);
+
   document.getElementById("msg").value = "";
+
+  // rolar pra baixo
+  chat.scrollTop = chat.scrollHeight;
 }
