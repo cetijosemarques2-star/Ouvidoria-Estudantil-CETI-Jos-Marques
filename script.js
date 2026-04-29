bd8cca9d3ab247c91b2e9406c14e8556914c1f5c
cetijosemarques2-star <cetijosemarques2@gmail.com>
2026-04-29 10:33:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,18 @@ function abrir(secao) {
   document.getElementById(secao).classList.add('active');
 }
 
-// Firebase config (coloque depois)
-const firebaseConfig = {
-  // sua config aqui
-};
-
-// Inicializar Firebase depois
-// const app = firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
-
-// Enviar formulário
 function enviar() {
   alert("Resposta enviada!");
 }
 
-// Chat simples (temporário)
 function enviarMsg() {
   let msg = document.getElementById("msg").value;
   let chat = document.getElementById("chatBox");
 
-  let p = document.createElement("p");
-  p.textContent = msg;
+  let div = document.createElement("div");
+  div.classList.add("msg", "aluno");
+  div.textContent = msg;
 
-  chat.appendChild(p);
+  chat.appendChild(div);
+  document.getElementById("msg").value = "";
 }
