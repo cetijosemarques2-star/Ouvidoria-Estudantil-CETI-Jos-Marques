949fba747034ea22cb0a8bf9141ae5fc92ad1674
cetijosemarques2-star <cetijosemarques2@gmail.com>
2026-04-30 16:02:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 function abrir(secao, botao){
-  document.querySelectorAll(".section").forEach(s=>s.classList.remove("active"));
+  document.querySelectorAll(".section").forEach(s=>{
+    s.classList.remove("active");
+  });
+
   document.getElementById(secao).classList.add("active");
 
-  document.querySelectorAll(".top-menu button").forEach(b=>b.classList.remove("active"));
+  document.querySelectorAll(".tabs button").forEach(b=>{
+    b.classList.remove("active");
+  });
+
   botao.classList.add("active");
 }
 
+/* CHAT */
 function enviarMsg(){
   const input = document.getElementById("msg");
   const chat = document.getElementById("chatBox");
 
-  if(input.value.trim()==="") return;
+  const texto = input.value.trim();
+  if(texto === "") return;
 
   const nome = document.createElement("div");
-  nome.className="nome";
-  nome.innerText="Anônimo";
+  nome.className = "nome";
+  nome.innerText = "Anônimo";
 
   const msg = document.createElement("div");
-  msg.className="msg aluno";
-  msg.innerText=input.value;
+  msg.className = "msg user";
+  msg.innerText = texto;
 
   chat.appendChild(nome);
   chat.appendChild(msg);
 
-  input.value="";
-  chat.scrollTop=chat.scrollHeight;
+  input.value = "";
+  chat.scrollTop = chat.scrollHeight;
 }
