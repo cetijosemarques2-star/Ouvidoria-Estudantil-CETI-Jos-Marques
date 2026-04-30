9df2f60ef86bb9779340174d644f9ce3c785c9e8
cetijosemarques2-star <cetijosemarques2@gmail.com>
2026-04-30 14:32:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function abrir(secao, botao) {
+  document.querySelectorAll(".section").forEach(s => s.classList.remove("active"));
+  document.getElementById(secao).classList.add("active");
+
+  document.querySelectorAll(".tab").forEach(b => b.classList.remove("active"));
+  botao.classList.add("active");
+}
+
 function enviarMsg() {
   const input = document.getElementById("msg");
   const chat = document.getElementById("chatBox");
@@ -18,7 +26,6 @@ function enviarMsg() {
 
   row.appendChild(user);
   row.appendChild(msg);
-
   chat.appendChild(row);
 
   input.value = "";
